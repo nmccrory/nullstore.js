9551d3b7ca9ec123de2729257edecd3814fa9c88
Nick McCrory <n.mccrory@icloud.com>
2017-06-30 13:37:59 -0700

    Updated mongoose configuration to point to mongolab sandbox.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/orders_customers');
+mongoose.connect('mongodb://APPSETTING_mlab_user:APPSETTING_mlab_pswd@ds032340.mlab.com:32340/order-management');
 
 var models_path = __dirname + '/../server/models';
 

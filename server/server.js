ec12450d4b96645a1769913175175b99e2153f63
Nick McCrory <n.mccrory@icloud.com>
2017-06-30 13:49:20 -0700

    Created web.config files for Azure app environment.

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(express.static(path.join(__dirname, '../client/static')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-
-app.listen(8000, function(){
+var port = process.env.PORT || 80;
+app.listen(port, function(){
 	console.log('Server running on port 8000');
 })
 

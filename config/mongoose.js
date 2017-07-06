5c37d0af94dcf7c7d4373a12e6b129bb8e4e0325
Nick McCrory <n.mccrory@icloud.com>
2017-07-06 13:12:40 -0700

    Added useMongoClient option (set to true) to  mongoose.connect()

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 
-mongoose.connect('mongodb://APPSETTING_mlab_user:APPSETTING_mlab_pswd@ds032340.mlab.com:32340/order-management');
+mongoose.connect('mongodb://APPSETTING_mlab_user:APPSETTING_mlab_pswd@ds032340.mlab.com:32340/order-management', { useMongoClient: true });
 
 var models_path = __dirname + '/../server/models';
 
